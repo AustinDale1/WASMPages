cf3a514f17b28dfe455566858d2b15bf5ab4f0d9
Austin Dale <ad25016@georgiasouthern.edu>
2025-02-18 19:46:09 -0500

    one more time :)

--- build.sh
+++ build.sh
@@ -5,7 +5,10 @@ mkdir -p build
 cd build
 
 # Configure with CMake
-emcmake cmake ..
+emcmake cmake .. \
+    -DPLATFORM=Web \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_EXE_LINKER_FLAGS="-s USE_GLFW=3 -s ASYNCIFY -s TOTAL_MEMORY=67108864"
 
 # Build
 emmake make
@@ -13,6 +16,7 @@ emmake make
 # Create dist directory
 cd ..
 mkdir -p dist
-cp build/raylib_game.* dist/
+cp build/raylib_game.html dist/index.html
+cp build/raylib_game.js dist/
 cp build/raylib_game.wasm dist/
 cp build/raylib_game.data dist/
