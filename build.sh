c54b8682f7ff1d439a2e8fa97ca37bbfd6ca731a
Austin Dale <ad25016@georgiasouthern.edu>
2025-02-23 21:14:35 -0500

    quick check to see if build and CMakeLists are messsing it up

--- build.sh
+++ build.sh
@@ -1,17 +1,17 @@
-#!/bin/bash
+# #!/bin/bash
 
-# Create build directory if it doesn't exist
-mkdir -p build
+# # Create build directory if it doesn't exist
+# mkdir -p build
 
-# Compile with Emscripten
-emcc main.cpp \
-    -o build/raylib_game.html \
-    -s USE_GLFW=3 \
-    -s WASM=1 \
-    -s ASYNCIFY \
-    -s ASSERTIONS=1 \
-    -lraylib
+# # Compile with Emscripten
+# emcc main.cpp \
+#     -o build/raylib_game.html \
+#     -s USE_GLFW=3 \
+#     -s WASM=1 \
+#     -s ASYNCIFY \
+#     -s ASSERTIONS=1 \
+#     -lraylib
 
-# Serve the files (optional)
-cd build
-python -m http.server 8080
+# # Serve the files (optional)
+# cd build
+# # python -m http.server 8080
